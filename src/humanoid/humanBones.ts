fff3020371db33ba1595bd16519c16231eba0807
Sknight Qian <sknight127@sina.com>
2022-02-15 18:09:23 +0800

    Delete humanBones.ts

--- src/humanoid/humanBones.ts
+++
@@ -1,7 +0,0 @@
-import { VRMSchema } from '../types'
-import { HumanBone } from './humanBone'
-
-/**
- * This object is a object variant of [[VRMHumanBoneArray]], used internally in [[VRMHumanoid]].
- */
-export type HumanBones = { [name in VRMSchema.HumanoidBoneName]: HumanBone[] }
