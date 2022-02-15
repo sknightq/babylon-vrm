76470849848252957991b6f7c0afad8cdbb2cdda
Sknight Qian <sknight127@sina.com>
2022-02-15 18:10:34 +0800

    Update importer.ts

--- src/humanoid/importer.ts
+++ src/humanoid/importer.ts
@@ -1,10 +1,9 @@
-import * as BABYLON from '@babylonjs/core';
-import { GLTFLoader, IGLTF, INode } from '@babylonjs/loaders/glTF/2.0';
-import { VRMSchema } from '../types';
-import { HumanBone } from './humanBone';
-import { HumanBoneArray } from './humanBoneArray';
-import { HumanDescription } from './humanDescription';
-import { Humanoid } from './index';
+import * as BABYLON from '@babylonjs/core'
+import { GLTFLoader, } from '@babylonjs/loaders/glTF/2.0'
+import { VRMSchema } from '../types'
+import { HumanBone, HumanBoneArray } from './humanBone'
+import { HumanDescription } from './humanDescription'
+import { Humanoid } from './index'
 
 /**
  * An importer that imports a [[VRMHumanoid]] from a VRM extension of a GLTF.
@@ -16,27 +15,26 @@ export class HumanoidImporter {
    * @param gltf A parsed result of GLTF taken from GLTFLoader
    */
   public async import(loader: GLTFLoader): Promise<Humanoid | null> {
-    // const vrmExt: VRMSchema.VRM | undefined = gltf.parser.json.extensions?.VRM;
-    const vrmExt: VRMSchema.VRM | undefined = loader.gltf.extensions?.VRM;
+    const vrmExt: VRMSchema.VRM | undefined = loader.gltf.extensions?.VRM
     if (!vrmExt) {
-      return null;
+      return null
     }
 
-    const schemaHumanoid: VRMSchema.Humanoid | undefined = vrmExt.humanoid;
+    const schemaHumanoid: VRMSchema.Humanoid | undefined = vrmExt.humanoid
     if (!schemaHumanoid) {
-      return null;
+      return null
     }
 
-    const humanBoneArray: HumanBoneArray = [];
+    const humanBoneArray: HumanBoneArray = []
     if (schemaHumanoid.humanBones) {
       await Promise.all(
-        schemaHumanoid.humanBones.map(async (bone) => {
+        schemaHumanoid.humanBones.map(async bone => {
           if (!bone.bone || bone.node == null) {
-            return;
+            return
           }
 
           // const node = await gltf.parser.getDependency('node', bone.node);
-          const node = loader.babylonScene.transformNodes[bone.node]
+          const node = (loader.gltf.nodes as any)[bone.node]
 
           humanBoneArray.push({
             name: bone.bone,
@@ -45,11 +43,11 @@ export class HumanoidImporter {
               center: bone.center && new BABYLON.Vector3(bone.center.x, bone.center.y, bone.center.z),
               max: bone.max && new BABYLON.Vector3(bone.max.x, bone.max.y, bone.max.z),
               min: bone.min && new BABYLON.Vector3(bone.min.x, bone.min.y, bone.min.z),
-              useDefaultValues: bone.useDefaultValues,
-            }),
-          });
-        }),
-      );
+              useDefaultValues: bone.useDefaultValues
+            })
+          })
+        })
+      )
     }
 
     const humanDescription: HumanDescription = {
@@ -60,9 +58,9 @@ export class HumanoidImporter {
       upperLegTwist: schemaHumanoid.upperLegTwist,
       lowerLegTwist: schemaHumanoid.lowerLegTwist,
       feetSpacing: schemaHumanoid.feetSpacing,
-      hasTranslationDoF: schemaHumanoid.hasTranslationDoF,
-    };
+      hasTranslationDoF: schemaHumanoid.hasTranslationDoF
+    }
 
-    return new Humanoid(humanBoneArray, humanDescription);
+    return new Humanoid(humanBoneArray, humanDescription)
   }
 }
