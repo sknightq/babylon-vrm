24be8ed62d1b1ba6c7ffc333a0b72d0a091f7cf7
Sknight Qian <sknight127@sina.com>
2022-02-15 18:19:05 +0800

    Update importer.ts

--- src/meta/importer.ts
+++ src/meta/importer.ts
@@ -1,6 +1,6 @@
 import * as BABYLON from '@babylonjs/core';
 import { VRMSchema } from '../types';
-import { GLTFLoader } from '@babylonjs/loaders/glTF/2.0';
+import { GLTFLoader,ITexture } from '@babylonjs/loaders/glTF/2.0';
 import { Meta } from './index';
 import { Options } from './options';
 
@@ -18,8 +18,8 @@ export class MetaImporter {
   }
 
   public async import(loader: GLTFLoader): Promise<Meta | null> {
-    // const vrmExt: VRMSchema.VRM | undefined = gltf.parent.extensions.VRM;
-    const vrmExt: VRMSchema.VRM | undefined = loader.gltf.extensions.VRM;
+  
+    const vrmExt: VRMSchema.VRM | undefined = loader.gltf.extensions?.VRM;
     if (!vrmExt) {
       return null;
     }
@@ -29,11 +29,13 @@ export class MetaImporter {
       return null;
     }
 
-    // let texture: BABYLON.Texture | null | undefined;
-    // if (!this.ignoreTexture && schemaMeta.texture != null && schemaMeta.texture !== -1) {
-    //   texture = await gltf.parser.getDependency('texture', schemaMeta.texture);
+    // NOTE: The thumbnailImage instead of texture
+    // let texture: number| null | undefined;
+    // if (!this.ignoreTexture && schemaMeta.texture !== null && schemaMeta.texture !== -1 && loader.gltf.textures?.length) {
+    //   texture = loader.gltf.textures[schemaMeta.texture as number]
     // }
-
+    // TODO: modify the property of return by vrm-1.0
+    // https://github.com/vrm-c/vrm-specification/blob/master/specification/VRMC_vrm-1.0-beta/meta.md
     return {
       allowedUserName: schemaMeta.allowedUserName,
       author: schemaMeta.author,
@@ -44,7 +46,7 @@ export class MetaImporter {
       otherPermissionUrl: schemaMeta.otherPermissionUrl,
       reference: schemaMeta.reference,
       sexualUssageName: schemaMeta.sexualUssageName,
-      texture: undefined,
+      texture: schemaMeta.texture,
       title: schemaMeta.title,
       version: schemaMeta.version,
       violentUssageName: schemaMeta.violentUssageName,
