f83fde5d3def34e562acb52b171a3930699df8be
Sknight Qian <sknight127@sina.com>
2022-02-15 18:21:00 +0800

    Update importer.ts

--- src/importer.ts
+++ src/importer.ts
@@ -1,37 +1,35 @@
-
-  
-import { GLTFLoader } from '@babylonjs/loaders/glTF/2.0';
-import { ExpressionImporter } from './expression/importer';
-import { FirstPersonImporter } from './firstPerson/importer';
-import { HumanoidImporter } from './humanoid/importer';
-import { LookAtImporter } from './lookAt/importer';
+import { GLTFLoader } from '@babylonjs/loaders/glTF/2.0'
+import { ExpressionImporter } from './expression/importer'
+import { FirstPersonImporter } from './firstPerson/importer'
+import { HumanoidImporter } from './humanoid/importer'
+import { LookAtImporter } from './lookAt/importer'
 // import { MaterialImporter } from './material/importer';
-import { MetaImporter } from './meta/importer';
-import { SpringBoneImporter } from './springBone/importer';
-import { traverse } from './utils/traverse';
-import { VRM } from './vrm';
+import { MetaImporter } from './meta/importer'
+import { SpringBoneImporter } from './springBone/importer'
+import { traverse } from './utils/traverse'
+import { VRM } from './vrm'
 
 export interface ImporterOptions {
-  metaImporter?: MetaImporter;
-  lookAtImporter?: LookAtImporter;
-  humanoidImporter?: HumanoidImporter;
-  expressionImporter?: ExpressionImporter;
-  firstPersonImporter?: FirstPersonImporter;
+  metaImporter?: MetaImporter
+  lookAtImporter?: LookAtImporter
+  humanoidImporter?: HumanoidImporter
+  expressionImporter?: ExpressionImporter
+  firstPersonImporter?: FirstPersonImporter
   // materialImporter?: MaterialImporter;
-  springBoneImporter?: SpringBoneImporter;
+  springBoneImporter?: SpringBoneImporter
 }
 
 /**
  * An importer that imports a [[VRM]] from a VRM extension of a GLTF.
  */
- export class Importer {
-  protected readonly _metaImporter: MetaImporter;
-  protected readonly _expressionImporter: ExpressionImporter;
-  protected readonly _lookAtImporter: LookAtImporter;
-  protected readonly _humanoidImporter: HumanoidImporter;
-  protected readonly _firstPersonImporter: FirstPersonImporter;
+export class Importer {
+  protected readonly _metaImporter: MetaImporter
+  protected readonly _expressionImporter: ExpressionImporter
+  protected readonly _lookAtImporter: LookAtImporter
+  protected readonly _humanoidImporter: HumanoidImporter
+  protected readonly _firstPersonImporter: FirstPersonImporter
   // protected readonly _materialImporter: MaterialImporter;
-  protected readonly _springBoneImporter: SpringBoneImporter;
+  protected readonly _springBoneImporter: SpringBoneImporter
 
   /**
    * Create a new VRMImporter.
@@ -39,27 +37,26 @@ export interface ImporterOptions {
    * @param options [[VRMImporterOptions]], optionally contains importers for each component
    */
   public constructor(options: ImporterOptions = {}) {
-    this._metaImporter = options.metaImporter || new MetaImporter();
-    this._expressionImporter = options.expressionImporter || new ExpressionImporter();
-    this._lookAtImporter = options.lookAtImporter || new LookAtImporter();
-    this._humanoidImporter = options.humanoidImporter || new HumanoidImporter();
-    this._firstPersonImporter = options.firstPersonImporter || new FirstPersonImporter();
+    this._metaImporter = options.metaImporter || new MetaImporter()
+    this._expressionImporter = options.expressionImporter || new ExpressionImporter()
+    this._lookAtImporter = options.lookAtImporter || new LookAtImporter()
+    this._humanoidImporter = options.humanoidImporter || new HumanoidImporter()
+    this._firstPersonImporter = options.firstPersonImporter || new FirstPersonImporter()
     // this._materialImporter = options.materialImporter || new MaterialImporter();
-    this._springBoneImporter = options.springBoneImporter || new SpringBoneImporter();
+    this._springBoneImporter = options.springBoneImporter || new SpringBoneImporter()
   }
 
   /**
    * Receive a GLTF loader retrieved from `@babylonjs/loaders/glTF/2.0` and create a new [[VRM]] instance.
    *
-   * @param loader A GLTFLoader which store the gltf object 
+   * @param loader A GLTFLoader which store the gltf object
    */
   public async import(loader: GLTFLoader): Promise<VRM> {
     if (loader.gltf.extensions === undefined || loader.gltf.extensions.VRM === undefined) {
-      throw new Error('Could not find VRM extension on the GLTF');
+      throw new Error('Could not find VRM extension on the GLTF')
     }
     // const scene = gltf.scene;
     // scene.updateMatrixWorld(false);
-
 
     // TODO: The frustumCulled of mesh will get rendered every frame even if it is not in the frustum of the camera. The Threejs default value is true to avioding external rendered.
     // Babylon set the defualt value will not passed to GPU, it's done CPU side.
@@ -74,26 +71,21 @@ export interface ImporterOptions {
     //   }
     // });
 
+    const meta = (await this._metaImporter.import(loader)) || undefined
 
-    const meta = (await this._metaImporter.import(loader)) || undefined;
-
+    // mToon动画材质
     // const materials = (await this._materialImporter.convertGLTFMaterials(loader)) || undefined;
     // https://github.com/virtual-cast/babylon-mtoon-material
-    // mToon动画材质
-    const materials = undefined
 
-    const humanoid = (await this._humanoidImporter.import(loader)) || undefined;
+    const humanoid = (await this._humanoidImporter.import(loader)) || undefined
 
-    const firstPerson = humanoid ? (await this._firstPersonImporter.import(loader, humanoid)) || undefined : undefined;
+    const firstPerson = humanoid ? (await this._firstPersonImporter.import(loader, humanoid)) || undefined : undefined
 
-    const expressionProxy = (await this._expressionImporter.import(loader)) || undefined;
+    const expressionProxy = (await this._expressionImporter.import(loader)) || undefined
 
-    const lookAt =
-      firstPerson && expressionProxy && humanoid
-        ? (await this._lookAtImporter.import(loader, firstPerson, expressionProxy, humanoid)) || undefined
-        : undefined;
+    const lookAt = firstPerson && expressionProxy && humanoid ? (await this._lookAtImporter.import(loader, firstPerson, expressionProxy, humanoid)) || undefined : undefined
 
-    const springBoneManager = (await this._springBoneImporter.import(loader)) || undefined;
+    const springBoneManager = (await this._springBoneImporter.import(loader)) || undefined
 
     return new VRM({
       scene: loader.babylonScene,
@@ -103,7 +95,7 @@ export interface ImporterOptions {
       firstPerson,
       expressionProxy,
       lookAt,
-      springBoneManager,
-    });
+      springBoneManager
+    })
   }
 }
