70ba86e4f4a96e6d959648df553199d07efff79e
Sknight Qian <sknight127@sina.com>
2022-02-17 18:01:04 +0800

    Update colliderGroup.ts

--- src/springBone/colliderGroup.ts
+++ src/springBone/colliderGroup.ts
@@ -1,7 +1,7 @@
 import * as BABYLON from '@babylonjs/core'
 
 /**
- * ColliderMesh, is actually just a `THREE.Mesh`.
+ * ColliderMesh, is actually just a `BABYLON.Mesh`.
  * Its radius and world position will be used for collisions.
  */
 export type SpringBoneColliderMesh = BABYLON.Mesh;
@@ -9,7 +9,7 @@ export type SpringBoneColliderMesh = BABYLON.Mesh;
 /**
  * A group of colliders, equivalents to an element of `colliderGroups` field of VRM schema.
  *
- * @see https://github.com/vrm-c/UniVRM/blob/master/specification/0.0/schema/vrm.secondaryanimation.collidergroup.schema.json
+ * @see https://github.com/vrm-c/vrm-specification/blob/084c4fd3a1/specification/0.0/schema/vrm.secondaryanimation.collidergroup.schema.json
  */
 export interface SpringBoneColliderGroup {
   node: number;
