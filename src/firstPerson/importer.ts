6967d36f312a259e2523702ed2344a7f6a32203c
Sknight Qian <sknight127@sina.com>
2022-02-21 17:48:48 +0800

    Update importer.ts

--- src/firstPerson/importer.ts
+++ src/firstPerson/importer.ts
@@ -1,5 +1,5 @@
 import * as BABYLON from '@babylonjs/core'
-import { GLTFLoader } from '@babylonjs/loaders/glTF/2.0'
+import { GLTFLoader, INode } from '@babylonjs/loaders/glTF/2.0'
 import { Humanoid } from '../humanoid'
 import { GLTFNode, GLTFSchema, VRMSchema } from '../types'
 import { gltfExtractPrimitivesFromNodes } from '../utils/gltfExtractPrimitivesFromNode'
@@ -44,7 +44,8 @@ export class FirstPersonImporter {
       ? new BABYLON.Vector3(
           schemaFirstPerson.firstPersonBoneOffset.x,
           schemaFirstPerson.firstPersonBoneOffset.y,
-          -schemaFirstPerson.firstPersonBoneOffset.z! // VRM 0.0 uses left-handed y-up
+          // IMPORTANT: VRM 0.0 uses left-handed y-up (same as BJS)
+          schemaFirstPerson.firstPersonBoneOffset.z! 
         )
       : new BABYLON.Vector3(0.0, 0.06, 0.0) // fallback, taken from UniVRM implementation
 
@@ -52,12 +53,12 @@ export class FirstPersonImporter {
     const nodePrimitivesMap = await gltfExtractPrimitivesFromNodes(loader)
 
     Array.from(nodePrimitivesMap.entries()).forEach(([nodeIndex, primitives]) => {
-      const schemaNode: GLTFSchema.Node = loader.gltf.nodes[nodeIndex]
+      const schemaNode: GLTFSchema.Node = (loader.gltf.nodes as INode[])[nodeIndex]
 
       const flag = schemaFirstPerson.meshAnnotations ? schemaFirstPerson.meshAnnotations.find(a => a.mesh === schemaNode.mesh) : undefined
       meshAnnotations.push(new RendererFirstPersonFlags(flag?.firstPersonFlag, primitives))
     })
 
-    return new FirstPerson(firstPersonBone, firstPersonBoneOffset, meshAnnotations)
+    return new FirstPerson(firstPersonBone, firstPersonBoneOffset, meshAnnotations, loader)
   }
 }
