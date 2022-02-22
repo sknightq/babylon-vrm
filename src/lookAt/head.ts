041651444b195b89ff019b6432d578043c5a91ab
Sknight Qian <sknight127@sina.com>
2022-02-22 17:38:02 +0800

    Update head.ts

--- src/lookAt/head.ts
+++ src/lookAt/head.ts
@@ -1,46 +1,47 @@
 import * as BABYLON from '@babylonjs/core'
-import { FirstPerson } from '../firstPerson';
+import { FirstPerson } from '../firstPerson'
+import { multiplyQuaternionByVectorToRef } from '../utils'
 // import { getWorldQuaternionLite } from '../utils/math';
-import { quatInvertCompat } from '../utils/quatInvertCompat';
-import { LookAtApplyer } from './applyer';
+// import { quatInvertCompat } from '../utils/quatInvertCompat';
+import { LookAtApplyer } from './applyer'
 
-const VECTOR3_FRONT = Object.freeze(new BABYLON.Vector3(0.0, 0.0, -1.0));
+const VECTOR3_FRONT = Object.freeze(new BABYLON.Vector3(0.0, 0.0, -1.0))
 
-const _v3A = new BABYLON.Vector3();
-const _v3B = new BABYLON.Vector3();
-const _v3C = new BABYLON.Vector3();
-const _quat = new BABYLON.Quaternion();
+const _v3A = new BABYLON.Vector3()
+const _v3B = new BABYLON.Vector3()
+const _v3C = new BABYLON.Vector3()
+const _quat = new BABYLON.Quaternion()
 
 /**
  * A class represents look at of a VRM.
  */
 export class LookAtHead {
-  public static readonly EULER_ORDER = 'YXZ'; // yaw-pitch-roll
+  public static readonly EULER_ORDER = 'YXZ' // yaw-pitch-roll
 
   /**
    * Associated [[VRMFirstPerson]], will be used for direction calculation.
    */
-  public readonly firstPerson: FirstPerson;
+  public readonly firstPerson: FirstPerson
 
   /**
    * Associated [[VRMLookAtApplyer]], its look at direction will be applied to the model using this applier.
    */
-  public readonly applyer?: LookAtApplyer;
+  public readonly applyer?: LookAtApplyer
 
   /**
    * If this is true, its look at direction will be updated automatically by calling [[VRMLookAtHead.update]] (which is called from [[VRM.update]]).
    *
    * See also: [[VRMLookAtHead.target]]
    */
-  public autoUpdate = true;
+  public autoUpdate = true
 
   /**
    * The target object of the look at.
    * Note that it does not make any sense if [[VRMLookAtHead.autoUpdate]] is disabled.
    */
-  public target?: BABYLON.TransformNode;
+  public target?: BABYLON.TransformNode
 
-  protected _euler: BABYLON.Vector3 = new BABYLON.Vector3(0.0, 0.0, 0.0);
+  protected _euler: BABYLON.Vector3 = new BABYLON.Vector3(0.0, 0.0, 0.0)
 
   /**
    * Create a new VRMLookAtHead.
@@ -49,19 +50,23 @@ export class LookAtHead {
    * @param applyer A [[VRMLookAtApplyer]] that will be associated with this new VRMLookAtHead
    */
   constructor(firstPerson: FirstPerson, applyer?: LookAtApplyer) {
-    this.firstPerson = firstPerson;
-    this.applyer = applyer;
+    this.firstPerson = firstPerson
+    this.applyer = applyer
   }
 
   /**
    * Get its look at direction in world coordinate.
    *
-   * @param target A target `THREE.Vector3`
+   * @param target A target `BABYLON.Vector3`
    */
   public getLookAtWorldDirection(target: BABYLON.Vector3): BABYLON.Vector3 {
     // const rot = getWorldQuaternionLite(this.firstPerson.firstPersonBone, _quat);
     // return target.copy(VECTOR3_FRONT).applyEuler(this._euler).applyQuaternion(rot);
-    return target.copyFrom(VECTOR3_FRONT)
+    this.firstPerson.firstPersonBone.getWorldMatrix().decompose(...[,], _quat)
+    const quaternionElur = BABYLON.Quaternion.FromEulerVector(this._euler)
+    multiplyQuaternionByVectorToRef(quaternionElur, target.copyFrom(VECTOR3_FRONT), target)
+    multiplyQuaternionByVectorToRef(_quat, target, target)
+    return target
   }
 
   /**
@@ -71,10 +76,10 @@ export class LookAtHead {
    * @param position A target position
    */
   public lookAt(position: BABYLON.Vector3): void {
-    this._calcEuler(this._euler, position);
+    this._calcEuler(this._euler, position)
 
     if (this.applyer) {
-      this.applyer.lookAt(this._euler);
+      this.applyer.lookAt(this._euler)
     }
   }
 
@@ -85,29 +90,36 @@ export class LookAtHead {
    * @param delta deltaTime
    */
   public update(delta: number): void {
+    if (delta <= 0) {
+      return
+    }
     if (this.target && this.autoUpdate) {
       _v3A.copyFrom(this.target.getAbsolutePosition())
-      this.lookAt(_v3A);
+      this.lookAt(_v3A)
 
       if (this.applyer) {
-        this.applyer.lookAt(this._euler);
+        this.applyer.lookAt(this._euler)
       }
     }
   }
 
   protected _calcEuler(target: BABYLON.Vector3, position: BABYLON.Vector3): BABYLON.Vector3 {
-    const headPosition = this.firstPerson.getFirstPersonWorldPosition(_v3B);
+    const headPosition = this.firstPerson.getFirstPersonWorldPosition(_v3B)
 
     // Look at direction in world coordinate
-    const lookAtDir = _v3C.copyFrom(position).subtract(headPosition).normalize();
+    const lookAtDir = _v3C.copyFrom(position).subtract(headPosition).normalize()
 
     // Transform the direction into local coordinate from the first person bone
     // lookAtDir.applyQuaternion(quatInvertCompat(getWorldQuaternionLite(this.firstPerson.firstPersonBone, _quat)));
+    // TODO: quaternion * lookAtDir
+    this.firstPerson.firstPersonBone.getWorldMatrix().decompose(...[,], _quat)
+    BABYLON.Quaternion.InverseToRef(_quat, _quat)
+    multiplyQuaternionByVectorToRef(_quat, lookAtDir, lookAtDir)
 
     // convert the direction into euler
-    target.x = Math.atan2(lookAtDir.y, Math.sqrt(lookAtDir.x * lookAtDir.x + lookAtDir.z * lookAtDir.z));
-    target.y = Math.atan2(-lookAtDir.x, -lookAtDir.z);
+    target.x = Math.atan2(lookAtDir.y, Math.sqrt(lookAtDir.x * lookAtDir.x + lookAtDir.z * lookAtDir.z))
+    target.y = Math.atan2(-lookAtDir.x, -lookAtDir.z)
 
-    return target;
+    return target
   }
 }
